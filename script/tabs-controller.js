76abb9c60582b2a72f746c35e06853832d476415
hasithwijerathna <has.ucsc@gmail.com>
2016-07-21 08:03:58 +0530

    Add tab directive.

--- script/tabs-controller.js
+++ script/tabs-controller.js
@@ -1,12 +1,24 @@
 'use strict';
 
-//control main tabs
-App.controller('tabController',function($scope){
-    var tab = 1;
-    $scope.selectTab = function(value){
-        tab = value;
+App.controller('tabController', function($scope){
+    $scope.tab = 1;
+
+    $scope.selectTab = function(setTab){
+        $scope.tab = setTab;
     }
-    $scope.isSelectedTab = function(value){
-        return tab === value;
+
+    $scope.isSelectedTab = function(checkTab){
+        return $scope.tab === checkTab;
     }
+});
+
+//we have to use tabPanels in the html page element like <tab-panels>
+//tabPanels --> <tab-panels>
+//if not it will not working.
+App.directive('tabPanels', function(){
+    return {
+        restrict : 'E', //can use as a Element.
+        templateUrl : 'tab-panels.html',
+        controller : 'tabController'
+    };
 });
